71f6e81f664f43670156e0936e606e17f8e964be
Matthew Cavignac <matthewcavignac@googlemail.com>
2018-11-19 15:23:07 +0000

    inital project commit

---
+++ Luke Baldwin ScreamBeam/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerRotation: MonoBehaviour
+{
+
+
+	void Update()
+    {
+        //Gives Character a Directional value of where it should be looking in comparision with the mouse
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+}
