1753949e641879bd0f58403575dfc1dfe162b7d0
BadatgamesLuke <44369470+BadatgamesLuke@users.noreply.github.com>
2019-01-11 11:59:19 +0000

    Final Commit

--- Luke Baldwin ScreamBeam/Assets/Scripts/Settings.cs
+++ Luke Baldwin ScreamBeam/Assets/Scripts/Settings.cs
@@ -6,31 +6,15 @@ using UnityEngine.UI;
 public class Settings : MonoBehaviour
 {
 
-    Resolution[] resolutions;
-    public Dropdown resolutionDropdown;
 
-    void Start()
+ public void SetQuality (int qualityIndex)
     {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if (Screen.currentResolution.width == resolutions[i].width &&
-              Screen.currentResolution.height == resolutions[i].height)
-            {
-                currentResolutionIndex = i;
-
-
-
-            }
-        }
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
-}
+
+
+          }
+
+        
+   
 
